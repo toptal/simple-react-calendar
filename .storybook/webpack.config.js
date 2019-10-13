f3025ada14f021a3d3fcfa3bac7f2e6cda2486c1
Peter Marosi <peter.marosi@toptal.com>
2019-10-13 10:56:21 +0200

    feat(typescript): add typescript support to the repo

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,24 +1,33 @@
-const path = require('path')
+const autoprefixer = require('autoprefixer')
 
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
+module.exports = ({
+  config
+}) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx|js|jsx)$/,
+    loader: require.resolve('babel-loader'),
+  })
 
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
-
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.styl$/,
-        loaders: ['style-loader', 'css-loader', 'stylus-loader'],
-        include: path.resolve(__dirname, '../'),
+  config.module.rules.push({
+    test: /\.styl$/,
+    use: [{
+        loader: require.resolve('style-loader'),
       },
-    ],
-  },
+      {
+        loader: require.resolve('postcss-loader'),
+        options: {
+          ident: 'postcss',
+          sourceMap: false,
+          plugins: [autoprefixer],
+        },
+      },
+      {
+        loader: require.resolve('stylus-loader'),
+      },
+    ]
+  })
+
+  config.resolve.extensions.push('.ts', '.tsx', '.js', '.jsx')
+
+  return config
 }
