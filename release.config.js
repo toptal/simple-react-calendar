b7d98525e161f494182d4fef1e764bcfe0993ee3
Peter Marosi <peter.marosi@toptal.com>
2020-02-08 21:51:52 +0100

    fix(ci): fix semantic versioning (#130)

--- release.config.js
+++ release.config.js
@@ -4,8 +4,7 @@ module.exports = {
   analyzeCommits: ['@semantic-release/commit-analyzer'], // Determine the type of release by analyzing commits with conventional-changelog
   verifyConditions: [
     '@semantic-release/changelog', // Verify the presence and the validity of the configuration
-    '@semantic-release/npm', // Verify the presence and the validity of the npm authentication and release configuration
-    '@semantic-release/github', // Verify the presence and the validity of the GitHub authentication and release configuration
+
     '@semantic-release/git' // Verify the presence and the validity of the Git authentication and release configuration
   ],
   generateNotes: ['@semantic-release/release-notes-generator'], // Generate release notes for the commits added since the last release with conventional-changelog
@@ -15,27 +14,17 @@ module.exports = {
     [
       '@semantic-release/exec', // Execute a shell command to bump next version inside README.md / package.json
       {
-        generateNotesCmd: './config/_ci/release/bump-version ${nextRelease.version}',
-        prepareCmd: './config/_ci/release/prepare-assets ${nextRelease.version}'
-      }
-    ],
-    [
-      '@semantic-release/exec', // Execute a shell command generate js bundle
-      {
         prepareCmd: 'yarn build && yarn build:lib'
-      }
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'README.md', 'package.json', './dist', './lib', './docs'] // Push a release commit and tag, including globed files
       }
     ]
   ],
   publish: [
     '@semantic-release/npm', // Update the package.json version and create the npm package tarball
-    ,
-    '@semantic-release/github', // Publish a GitHub release
+    ['@semantic-release/github', {
+      "assets": [
+        {"path": "dist/simple-calendar.js", "label": "Standalone JS file"}
+      ]
+    }]// Publish a GitHub release
   ],
   success: ['@semantic-release/github'], // Add a comment to GitHub issues and pull requests resolved in the release
   fail: ['@semantic-release/github'], // Open a GitHub issue when a release fails
